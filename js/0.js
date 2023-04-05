c263bb88363e02abe95d18c559b6d98c564323ca
Vivek Kumar Singh <vivek.rbj.up@gmail.com>
2023-04-05 15:44:52 +0530

    created homepage

---
+++ js/0.js
@@ -0,0 +1,16 @@
+let a = document.getElementById("login")
+let b = document.getElementById("register")
+let c = document.getElementById("btn")
+// let c = document.getElementById("all_i")
+
+function login(){
+    a.style.left = "0px"
+    b.style.left = "450px"
+    c.style.left = "0px"
+    // a.innerText = "hi"
+}
+function register(){
+    a.style.left = "-450px"
+    b.style.left = "0px"
+    c.style.left = "110px"
+}
